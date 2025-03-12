e855bb9885714408881a93c947e1ac9313402cc1
Tianzhengshuyuan <298208490@qq.com>
2025-03-12 10:02:05 +0800

    批量删除名字包含A3的文件夹和名字包含原卷的文件

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# 删除所有名字中包含“A3”的文件夹
+find . -type d -name '*A3*' -exec rm -rf {} +
+
+# 删除所有名字中包含“原卷”的文件
+find . -type f -name '*原卷*' -exec rm -f {} +
+
 # 从命令行获取参数
 DOCX_NAME=$1  # 第一个参数是 docx 文件名
 LATEX_OPTION=$2  # 第二个参数是 latex 的选项
